244d679269b88f131b5f1c7a2416d08cb705c449
Amish <amishpatel94@hotmail.com>
2016-07-10 19:46:29 -0400

    Implemented basic UI elements

--- app/src/main/java/com/voice/voice/MainActivity.java
+++ app/src/main/java/com/voice/voice/MainActivity.java
@@ -2,12 +2,25 @@ package com.voice.voice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RadioGroup languageSwitch;
+    private EditText transcriptView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        languageSwitch = (RadioGroup) findViewById(R.id.toggleLanguageSetting);
+        transcriptView = (EditText) findViewById(R.id.transcriptEditText);
+        transcriptView.setKeyListener(null);
+        transcriptView.setEnabled(true);
     }
+
+
 }
