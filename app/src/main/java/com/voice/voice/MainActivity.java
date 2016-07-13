7baba0dc2a8ffd105bc8cf75c66cfe2f22d7a70a
Amish <amishpatel94@hotmail.com>
2016-07-13 16:07:11 -0400

    working TextToSpeech module

--- app/src/main/java/com/voice/voice/MainActivity.java
+++ app/src/main/java/com/voice/voice/MainActivity.java
@@ -1,18 +1,22 @@
 package com.voice.voice;
 
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.EditText;
 import android.widget.RadioGroup;
 import android.widget.Switch;
 
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
 
     private RadioGroup languageSwitch;
     private EditText transcriptView;
+    private TextToSpeech tts;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -20,6 +24,19 @@ public class MainActivity extends AppCompatActivity {
         transcriptView = (EditText) findViewById(R.id.transcriptEditText);
         transcriptView.setKeyListener(null);
         transcriptView.setEnabled(true);
+        final Bundle bun = savedInstanceState;
+
+        tts = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                String toSpeak = transcriptView.getText().toString();
+                tts.setLanguage(Locale.US);
+
+                tts.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, bun, null);
+            }
+        });
+
+
     }
 
 
