6e8616b4aac773d1c0ea12f2769a8ed4803debdf
Amish <amishpatel94@hotmail.com>
2016-07-14 14:40:30 -0400

    Implemented UI listeners and initialized view to default state on startup

--- app/src/main/java/com/voice/voice/MainActivity.java
+++ app/src/main/java/com/voice/voice/MainActivity.java
@@ -7,8 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.RadioGroup;
-import android.widget.Switch;
 import android.widget.Toast;
 
 import java.io.File;
@@ -19,23 +19,49 @@ import java.util.Locale;
 public class MainActivity extends AppCompatActivity {
 
     private RadioGroup languageSwitch;
+    private RadioButton ASLSetting;
+    private RadioButton JSLSetting;
+    private RadioGroup connectionsSwitch;
+    private RadioButton connectionOnSetting;
+    private RadioButton connectionOffSetting;
     private EditText transcriptView;
+    private Button clearTranscriptButton;
     private Button exportTxtButton;
     private TextToSpeech tts;
+
+    private boolean aslSelected;
+    private boolean connectedToGloves;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // This initializes the Language settings
         languageSwitch = (RadioGroup) findViewById(R.id.toggleLanguageSetting);
-        
+        ASLSetting = (RadioButton) findViewById(R.id.ASLSetting);
+        JSLSetting = (RadioButton) findViewById(R.id.JSLSetting);
+        aslSelected = languageSwitch.getCheckedRadioButtonId() == ASLSetting.getId();
+        languageSwitch.setOnCheckedChangeListener(languageListener);
+
+        // This initializes the Connections settings
+        connectionsSwitch = (RadioGroup) findViewById(R.id.toggleConnectionSetting);
+        connectionOnSetting = (RadioButton) findViewById(R.id.connectionOnSetting);
+        connectionOffSetting = (RadioButton) findViewById(R.id.connectionOffSetting);
+        connectionsSwitch.setOnCheckedChangeListener(connectionListener);
+
+        // Transcript view initialization
         transcriptView = (EditText) findViewById(R.id.transcriptEditText);
         transcriptView.setKeyListener(null);
         transcriptView.setEnabled(true);
-        exportTxtButton = (Button) findViewById(R.id.exportToTxtButton);
-        final Bundle bun = savedInstanceState;
+        clearTranscriptButton = (Button) findViewById(R.id.clearTranscriptButton);
+        clearTranscriptButton.setOnClickListener(clearTranscriptListener);
 
+        // Export button initialization
+        exportTxtButton = (Button) findViewById(R.id.exportToTxtButton);
+        exportTxtButton.setOnClickListener(exportButtonListener);
+
+        final Bundle bun = savedInstanceState;
         // Text To Speech Module Initializtion
         tts = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
             @Override
@@ -47,12 +73,42 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        exportTxtButton.setOnClickListener(exportButtonListener);
-
-
     }
 
-    // Export the
+    RadioGroup.OnCheckedChangeListener languageListener = new RadioGroup.OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(RadioGroup group, int checkedId) {
+            aslSelected = languageSwitch.getCheckedRadioButtonId() == ASLSetting.getId();
+            if (aslSelected) {
+                Toast.makeText(getApplicationContext(), "ASL Selected", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getApplicationContext(), "JSL Selected", Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
+
+    RadioGroup.OnCheckedChangeListener connectionListener = new RadioGroup.OnCheckedChangeListener() {
+        @Override
+        public void onCheckedChanged(RadioGroup group, int checkedId) {
+            connectedToGloves = connectionsSwitch.getCheckedRadioButtonId() == connectionOnSetting.getId();
+            if (connectedToGloves) {
+                Toast.makeText(getApplicationContext(), "Connected to the gloves", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getApplicationContext(), "Disconnected from the gloves", Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
+
+    View.OnClickListener clearTranscriptListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            transcriptView.setText("");
+            Toast.makeText(getApplicationContext(),"Successfully exported text file to Downloads Folder",
+                    Toast.LENGTH_SHORT).show();
+        }
+    };
+
+
     View.OnClickListener exportButtonListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
