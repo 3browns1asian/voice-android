42fb51f5f336e770150b2628525a3fe198ee459c
Amish <amishpatel94@hotmail.com>
2017-03-05 13:03:37 -0500

    Adds back JSL support and connection on/off works now

--- app/src/main/java/com/voice/voice/MainActivity.java
+++ app/src/main/java/com/voice/voice/MainActivity.java
@@ -89,9 +89,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        // SetInterval to handle stream of data
-//        speechArr = speech.split(" ");
-
 
         bluetoothSetup();
 
@@ -99,12 +96,7 @@ public class MainActivity extends AppCompatActivity {
         serverWrapper.getmSocket().on("connect", new Emitter.Listener() {
             @Override
             public void call(Object... args) {
-                String[] simData = simulateArduinoData();
-                for (String str:simData){
-//                    serverWrapper.sendArduinoData(str);
-//                    bluetoothSetup();
-                }
-                Log.d("connected","yo");
+                Log.d("Server Connected","yo");
             }
         }).on("predictedValue", new Emitter.Listener() {
             @Override
@@ -121,34 +113,6 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    // TODO: Remove all this simulation stuff after
-    public String[] simulateArduinoData() {
-        String[] helloData = { "-8.30,1.36,5.66,0.07,-0.03,-0.05,346,358,355,337,352|0,0,0,0,0,0,0,0,0,0,0",
-                "-8.89,0.85,6.21,0.01,0.10,0.19,346,357,355,336,351|0,0,0,0,0,0,0,0,0,0,0",
-                "-8.23,2.46,2.84,0.46,0.28,-0.62,345,359,355,335,347|0,0,0,0,0,0,0,0,0,0,0",
-                "-8.74,0.84,2.97,0.29,-0.22,-0.65,343,359,354,335,344|0,0,0,0,0,0,0,0,0,0,0",
-                "-9.63,-1.81,4.16,0.14,-0.23,-0.49,345,359,356,335,347|0,0,0,0,0,0,0,0,0,0,0",
-                "-9.43,-2.98,4.68,-0.15,0.07,0.06,342,359,354,335,347|0,0,0,0,0,0,0,0,0,0,0",
-                "-9.40,-1.66,3.57,-0.31,0.18,0.26,344,359,355,336,345|0,0,0,0,0,0,0,0,0,0,0",
-                "-10.06,-0.32,3.07,-0.65,0.18,0.04,344,360,356,336,348|0,0,0,0,0,0,0,0,0,0,0",
-                "-9.04,4.12,3.17,-0.20,0.02,0.65,344,360,355,336,350|0,0,0,0,0,0,0,0,0,0,0",
-                "-9.52,2.72,3.12,-0.02,-0.05,-0.07,344,359,355,336,349|0,0,0,0,0,0,0,0,0,0,0",
-                "-9.60,1.50,2.94,-0.04,-0.01,-0.13,343,359,355,336,348|0,0,0,0,0,0,0,0,0,0,0",
-                "END"};
-        String[] byeData = { "-7.26,-1.18,6.42,-0.01,-0.03,-0.11,357,372,368,379,373|0,0,0,0,0,0,0,0,0,0,0",
-                "-7.15,-1.22,6.67,-0.04,0.00,-0.06,357,371,369,379,374|0,0,0,0,0,0,0,0,0,0,0",
-                "-7.19,-0.93,6.13,0.01,0.11,-0.05,354,373,360,368,359|0,0,0,0,0,0,0,0,0,0,0",
-                "-7.62,-0.76,6.70,0.01,0.10,-0.02,436,379,402,437,424|0,0,0,0,0,0,0,0,0,0,0",
-                "-8.10,-1.37,5.35,-0.23,0.58,-0.07,525,419,519,505,521|0,0,0,0,0,0,0,0,0,0,0",
-                "-8.56,-0.91,5.18,0.03,0.09,-0.03,523,423,524,501,521|0,0,0,0,0,0,0,0,0,0,0",
-                "-8.31,-1.31,5.55,-0.03,-0.03,-0.04,520,421,522,498,519|0,0,0,0,0,0,0,0,0,0,0",
-                "-8.40,-1.41,5.36,-0.09,-0.00,-0.05,517,416,519,496,516|0,0,0,0,0,0,0,0,0,0,0",
-                "-8.20,-1.41,5.50,-0.04,-0.00,-0.05,516,413,517,494,515|0,0,0,0,0,0,0,0,0,0,0",
-                "-8.15,-1.54,5.57,-0.07,-0.00,-0.05,515,411,516,494,514|0,0,0,0,0,0,0,0,0,0,0",
-                "-8.18,-1.31,5.36,-0.06,-0.00,-0.03,514,410,514,493,512|0,0,0,0,0,0,0,0,0,0,0",
-                "END"};
-        return byeData;
-    }
 
     public void bluetoothSetup() {
         bt = new BluetoothSPP(getApplicationContext());
@@ -182,11 +146,15 @@ public class MainActivity extends AppCompatActivity {
             bt.setOnDataReceivedListener(new BluetoothSPP.OnDataReceivedListener() {
                 public void onDataReceived(byte[] data, String message) {
 //                    Log.d("Data Received", message);
-                    if (message.equals("END") && !sendData && bt.getConnectedDeviceAddress() != null) {
+                    if (message.equals("END") && connectedToGloves && !sendData && bt.getConnectedDeviceAddress() != null) {
                         sendData = true;
                     }
-                    if (sendData)
+                    if (sendData) {
                         serverWrapper.sendArduinoData(message.toString());
+                    }
+                    if (!connectedToGloves && message.equals("END")) {
+                        sendData = false;
+                    }
                 }
             });
         }
@@ -206,57 +174,21 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    public void runTroughTranscript() {
-        timer = new Timer();
-        timer.scheduleAtFixedRate(new TimerTask(){
-            int count = 0;
-            StringBuilder sent = new StringBuilder();
-
-            @Override
-            public void run(){
-                if (count < speechArr.length) {
-                    if (speechArr[count] == null) return;
-
-//                    Log.d("Message", speechArr[count]);
-                    tts.speak(speechArr[count],TextToSpeech.QUEUE_ADD, bun, null);
-                    sent.append(speechArr[count]);
-                    sent.append(" ");
-
-
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-//                            Log.d("String", sent.toString());
-                            transcriptView.setText(sent.toString());
-                        }
-                    });
-
-// This is there to buffering it into a sentence before saying it out
-//                    if ((speechArr[count].charAt(speechArr[count].length()-1)) == '.') {
-//                        Log.d("YO", sentence);
-//                        if (tts.isSpeaking()) {
-//                            tts.speak(sentence, TextToSpeech.QUEUE_ADD, bun, null);
-//                        } else {
-//                            tts.speak(sentence, TextToSpeech.QUEUE_FLUSH, bun, null);
-//                        }
-//                        sentence = "";
-//                    }
-                    count += 1;
-                }
-            }
-
-        },0,500);
-    }
-
     RadioGroup.OnCheckedChangeListener languageListener = new RadioGroup.OnCheckedChangeListener() {
         @Override
         public void onCheckedChanged(RadioGroup group, int checkedId) {
             aslSelected = languageSwitch.getCheckedRadioButtonId() == ASLSetting.getId();
             if (aslSelected) {
+                tts.setLanguage(Locale.US);
                 Toast.makeText(getApplicationContext(), "ASL Selected", Toast.LENGTH_SHORT).show();
+                serverWrapper.sendLanguageChangeNotification("ASL");
             } else {
+                tts.setLanguage(Locale.JAPAN);
                 Toast.makeText(getApplicationContext(), "JSL Selected", Toast.LENGTH_SHORT).show();
+                serverWrapper.sendLanguageChangeNotification("JSL");
             }
+            transcriptView.setText("");
+            speech = "";
         }
     };
 
@@ -277,8 +209,6 @@ public class MainActivity extends AppCompatActivity {
         public void onClick(View v) {
             transcriptView.setText("");
             speech = "";
-//            Toast.makeText(getApplicationContext(),"Successfully exported text file to Downloads Folder",
-//                    Toast.LENGTH_SHORT).show();
         }
     };
 
@@ -308,6 +238,5 @@ public class MainActivity extends AppCompatActivity {
                 }
         }
     };
-
 
 }
